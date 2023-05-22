26e7002955b1e4327c4e16b36fb53671e592d572
emeibech <emeibech@gmail.com>
2023-05-22 17:47:14 +0800

    Fix the other test cases for weather fn

--- tests/weather.test.js
+++ tests/weather.test.js
@@ -9,7 +9,7 @@ describe('weather unit test', () => {
     jest.resetAllMocks();
   });
 
-  it('makes GET requests with correct URLs and parameters', async () => {
+  it('makes GET requests with correct URL and parameters', async () => {
     const mockResponse = {
       body: {
         coord: {
@@ -29,31 +29,62 @@ describe('weather unit test', () => {
     await weather(mockReq);
 
     expect(needle).toHaveBeenCalledTimes(2);
+
+    // Assert the first needle call for weatherData
+    expect(needle).toHaveBeenCalledWith(
+      'get',
+      expect.stringMatching(
+        new RegExp(
+          `^${process.env.API_OW_WEATHER_URL}\\?${mockReq.url.slice(13)}&${process.env.API_OW_KEY_NAME}=${process.env.API_OW_KEY_VALUE}$`
+        )
+      )
+    );
+
+    // Assert the second needle call for forecastData
     expect(needle).toHaveBeenCalledWith(
       'get',
       expect.stringContaining(
-        `${process.env.API_OW_WEATHER_URL}?${mockReq.url.slice(13)}&${
-          process.env.API_OW_KEY_NAME
-        }=${process.env.API_OW_KEY_VALUE}`
+        `${process.env.API_OW_ONECALL_URL}?${process.env.API_OW_KEY_NAME}=${process.env.API_OW_KEY_VALUE}`
       )
     );
   });
 
-  it.skip('returns the response data', async () => {
-    const mockResponse = { body: { weatherData: 'Weather Data' } };
+  it('returns the response data', async () => {
+    const mockResponse = {
+      body: {
+        coord: {
+          lat: 14.6042,
+          lon: 120.9822,
+        },
+      },
+    };
+
     needle.mockResolvedValue(mockResponse);
 
-    const data = await weather();
+    const mockReq = {
+      headers: { host: 'localhost:3000' },
+      url: '/api/weather?q=manila',
+    };
 
-    expect(data).toEqual(mockResponse.body);
+    const data = await weather(mockReq);
+
+    expect(data).toEqual({
+      forecastData: mockResponse.body,
+      weatherData: mockResponse.body,
+    });
   });
 
-  it.skip('handles and logs errors', async () => {
-    const mockError = { body: { error: 'An error occured' } };
+  it('handles and logs errors', async () => {
+    const mockError = { body: { error: 'An error occurred' } };
     needle.mockRejectedValue(mockError);
     console.error = jest.fn();
 
-    await weather();
+    const mockReq = {
+      headers: { host: 'localhost:3000' },
+      url: '/api/weather?q=manila',
+    };
+
+    await weather(mockReq);
 
     expect(console.error).toHaveBeenCalledWith(mockError);
   });
