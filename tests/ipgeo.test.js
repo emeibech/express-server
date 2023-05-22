a34a9e051efd67a9070612106688be08144d07bb
emeibech <emeibech@gmail.com>
2023-05-22 08:04:24 +0800

    Test response data and error handling using mocks

--- tests/ipgeo.test.js
+++ tests/ipgeo.test.js
@@ -1,31 +1,45 @@
-const ipgeo = require("../routes/ipgeo");
-const needle = require("needle");
-require("dotenv").config();
+const ipgeo = require('../routes/ipgeo');
+const needle = require('needle');
+require('dotenv').config();
 
-jest.mock("needle");
+jest.mock('needle');
 
-describe("ipgeo unit test", () => {
+describe('ipgeo unit test', () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
 
-  it("Makes a GET request with correct URL and parameters", async () => {
-    const mockRes = {
-      body: {
-        weatherData: "Weather Data",
-      },
-    };
-
-    needle.mockResolvedValue(mockRes);
+  it('makes a GET request with correct URL and parameters', async () => {
+    const mockResponse = { body: { weatherData: 'Weather Data' } };
+    needle.mockResolvedValue(mockResponse);
 
     await ipgeo();
 
     expect(needle).toHaveBeenCalledTimes(1);
     expect(needle).toHaveBeenCalledWith(
-      "get",
+      'get',
       expect.stringContaining(
         `${process.env.API_IPGEO_URL}?${process.env.API_IPGEO_KEY_NAME}=${process.env.API_IPGEO_KEY_VALUE}`
       )
     );
+  });
+
+  it('returns the response data', async () => {
+    const mockResponse = { body: { weatherData: "Weather Data" } };
+    needle.mockResolvedValue(mockResponse);
+
+    const data = await ipgeo();
+
+    expect(data).toEqual(mockResponse.body);
+  });
+
+  it('handles and logs errors', async () => {
+    const mockError = { body: { error: "An error occured" } };
+    needle.mockRejectedValue(mockError);
+    console.error = jest.fn();
+
+    await ipgeo();
+
+    expect(console.error).toHaveBeenCalledWith(mockError);
   });
 });
