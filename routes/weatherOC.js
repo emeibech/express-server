6b2afcad4deae7e0efc5974d40c85b25575e8f59
emeibech <98073805+emeibech@users.noreply.github.com>
2023-05-29 16:53:24 +0800

    Create separate function for Onecall request

---
+++ routes/weatherOC.js
@@ -0,0 +1,28 @@
+const needle = require('needle');
+require('dotenv').config();
+
+const oneCallUrl = process.env.API_OW_ONECALL_URL;
+const keyName = process.env.API_OW_KEY_NAME;
+const keyValue = process.env.API_OW_KEY_VALUE;
+
+const weatherOC = async (req) => {
+  try {
+    const oneCallParams = new URL(req.url, `http://${req.headers.host}`)
+      .searchParams;
+
+    oneCallParams.append('exclude', 'minutely,hourly,alerts');
+    oneCallParams.append(keyName, keyValue);
+
+    const oneCallData = await needle('get', `${oneCallUrl}?${oneCallParams}`);
+
+    if (oneCallData.statusCode !== 200) return {
+      error: `${oneCallData.statusCode} ${oneCallData.statusMessage}`,
+    };
+
+    return oneCallData.body;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = weatherOC;
