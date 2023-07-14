bfbde2ed1ee736bb5d5031c6850fa8f4ca8f2be4
emeibech <98073805+emeibech@users.noreply.github.com>
2023-07-14 20:14:39 +0800

    Make general assistant summarize chat history upon reaching 2000 token estimate

--- routes/openai/generalAssistant.js
+++ routes/openai/generalAssistant.js
@@ -6,13 +6,16 @@ const history = ChatHistory('You are a helpful assistant.');
 const generalAssistant = async (req) => {
   if (req.query.reset) history.resetHistory();
 
-  return completionWithHistory({
+  const completion = await completionWithHistory({
     history: history.getHistory(),
     userContent: req.query.prompt,
     temperature: 0.5,
-    timeout: 15000,
     addEntry: history.addEntry,
   });
+
+  if (history.getTokenEstimate() > 2000) history.summarizeHistory();
+
+  return completion;
 };
 
 export default generalAssistant;
