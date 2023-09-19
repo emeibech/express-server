5bd42a510a5352baa756138b42c35f79aa36aa5e
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-20 06:17:45 +0800

    Import routes and define their paths

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import index from './routes/index.js';
+import ipgeo from './routes/openweather/ipgeo.js';
+import weather from './routes/openweather/weather.js';
+import onecall from './routes/openweather/onecall.js';
+import codeAnalyzer from './routes/openai/codeAnalyzer.js';
+import codingAssistant from './routes/openai/codingAssistant.js';
+import eli5 from './routes/openai/eli5.js';
+import storyGenerator from './routes/openai/storyGenerator.js';
+import toneChanger from './routes/openai/toneChanger.js';
+import generalAssistant from './routes/openai/generalAssistant.js';
 
 dotenv.config();
 
@@ -9,14 +18,25 @@ const app = express();
 
 app.set('trust proxy', 1);
 app.use('/', index);
+app.use('/ipgeo', ipgeo);
+app.use('/weather', weather);
+app.use('/onecall', onecall);
+app.use('/codeanalyzer', codeAnalyzer);
+app.use('/codingassistant', codingAssistant);
+app.use('/eli5', eli5);
+app.use('/storygenerator', storyGenerator);
+app.use('/tonechanger', toneChanger);
+app.use('/generalassistant', generalAssistant);
 
-// Error handling middleware
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
   console.error(err);
-  res.status(500).json({ error: 'Internal Server Error' });
+  res.status(500);
+  res.send("You don't have access to this endpoint");
 });
 
-// Start server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
