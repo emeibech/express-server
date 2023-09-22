32cabd07bd5c92e8b3f7c069c7c156fa0ca2a70b
emeibech <98073805+emeibech@users.noreply.github.com>
2023-09-22 23:49:52 +0800

    Put the error message in error handler middleware

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express, { NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import index from './routes/index.js';
+import ipgeo from './routes/openweather/ipgeo.js';
+import weather from './routes/openweather/weather.js';
+import onecall from './routes/openweather/onecall.js';
+import codeAnalyzer from './routes/openai/codeAnalyzer.js';
+import codingAssistant from './routes/openai/codingAssistant.js';
+import eli5 from './routes/openai/eli5.js';
+import storyGenerator from './routes/openai/storyGenerator.js';
+import toneChanger from './routes/openai/toneChanger.js';
+import generalAssistant from './routes/openai/generalAssistant.js';
+
+dotenv.config();
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+app.set('trust proxy', 1);
+app.use('/', index);
+app.use('/ipgeo', ipgeo);
+app.use('/weather', weather);
+app.use('/onecall', onecall);
+app.use('/codeanalyzer', codeAnalyzer);
+app.use('/codingassistant', codingAssistant);
+app.use('/eli5', eli5);
+app.use('/storygenerator', storyGenerator);
+app.use('/tonechanger', toneChanger);
+app.use('/generalassistant', generalAssistant);
+
+app.use((err: Error, _req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  console.error(err);
+  res.status(500);
+  res.json({
+    [err.name]: err.message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
