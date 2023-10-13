8f87a40f25f0d1b14ae63748272791ff7309b38d
emeibech <98073805+emeibech@users.noreply.github.com>
2023-10-13 09:42:20 +0800

    Add utils for noStream api calls

---
+++ src/routes/openai/utils/noStreamCompletion.ts
@@ -0,0 +1,27 @@
+import openai from './openaiConfig.js';
+import { ChatCompletionOptions } from './chatCompletion.js';
+
+const noStreamChatCompletion = async ({
+  res,
+  sysContent,
+  userContent,
+  temperature,
+  model,
+}: ChatCompletionOptions) => {
+  const stream = await openai.chat.completions.create({
+    temperature,
+    model: model || 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: sysContent,
+      },
+      ...userContent,
+    ],
+  });
+
+  const data = stream.choices[0].message.content;
+  res.json(data);
+};
+
+export default noStreamChatCompletion;
