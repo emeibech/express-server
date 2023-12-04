a3a87405e2ebeb5a05c9827367c250eb52408cdd
emeibech <98073805+emeibech@users.noreply.github.com>
2023-12-04 16:03:20 +0800

    Create db fns isEmailTaken and createNewUser

---
+++ src/database/users.ts
@@ -0,0 +1,40 @@
+import { CreateNewUser } from '@/types/database.js';
+import pool, { transaction } from './utils.js';
+
+export async function isEmailTaken(email: string) {
+  try {
+    const query = 'SELECT email FROM users WHERE email = $1;';
+    const values = [email];
+    const { rows } = await pool.query(query, values);
+
+    return rows.length > 0;
+  } catch (error) {
+    console.log('Something went wrong when checking email validity.');
+    throw error;
+  }
+}
+
+export async function createNewUser({
+  firstname,
+  email,
+  hashedPassword,
+  dateOfBirth,
+  lastname = null,
+}: CreateNewUser) {
+  try {
+    await transaction([
+      {
+        text: `
+          INSERT INTO users (firstname, lastname, email, password, date_of_birth)
+          VALUES ($1, $2, $3, $4, $5);
+        `,
+        values: [firstname, lastname, email, hashedPassword, dateOfBirth],
+      },
+    ]);
+
+    console.log('New user created');
+  } catch (error) {
+    console.log('Something went wrong when creating new user.');
+    throw error;
+  }
+}
