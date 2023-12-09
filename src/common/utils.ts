9156eeee13e2b7707b3e635b62a17f5676d56bff
emeibech <98073805+emeibech@users.noreply.github.com>
2023-12-09 18:18:49 +0800

    Create verifyToken util fn

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,4 +1,9 @@
 import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { getJwtError } from './getErrorMessage.js';
+
+dotenv.config();
 
 export async function hashPassword(password: string) {
   try {
@@ -22,4 +27,30 @@ export async function comparePasswords(
     console.error('Error comparing passwords:', error);
     throw error;
   }
+}
+
+export async function verifyToken(token: string) {
+  try {
+    const secret = process.env.JWT_SECRET || 'jyrf45gq978n_97YG4Q5';
+    const decoded = jwt.verify(token, secret);
+
+    return {
+      payload: decoded as JwtPayload,
+    };
+  } catch (error) {
+    const jwtError = getJwtError(error);
+    if (jwtError.includes('jwt expired')) return { expired: true };
+    return { error: getJwtError(error) };
+  }
+}
+
+export function decodeToken(token: string) {
+  const payload = token
+    .split('.')
+    .filter((_part, index) => index === 1)
+    .join('');
+
+  const buff = Buffer.from(payload, 'base64url');
+  const decoded = buff.toString('ascii');
+  return JSON.parse(decoded);
 }
