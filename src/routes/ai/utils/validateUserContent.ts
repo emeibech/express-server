e588e64def9c17bb5c8e7802ca358e2e8422b32c
emeibech <98073805+emeibech@users.noreply.github.com>
2023-12-12 23:12:45 +0800

    Create middleware for validating userContent

---
+++ src/routes/ai/utils/validateUserContent.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+import type { NextFunction, Request, Response } from 'express';
+
+const userContentSchema = z.array(
+  z.object({
+    role: z.enum(['user', 'assistant']),
+    content: z.string(),
+  }),
+);
+
+export function validateUserContent(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { userContent } = req.body;
+    const parsed = userContentSchema.safeParse(userContent);
+
+    if (!parsed.success) {
+      return res.status(400).json({
+        message:
+          "TypeError: userContent should be an array of objects. { role: 'user' | 'assistant', content: string }[]",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log('An error occured while validating userContent: ', error);
+  }
+}
