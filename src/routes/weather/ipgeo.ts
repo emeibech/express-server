8cc1bb2e6d21a766d6f8edd38288c12fcd68e67f
emeibech <98073805+emeibech@users.noreply.github.com>
2024-01-10 18:55:24 +0800

    Fix typescript error

--- src/routes/weather/ipgeo.ts
+++ src/routes/weather/ipgeo.ts
@@ -9,7 +9,7 @@ const keyValue = process.env.API_IPGEO_KEY_VALUE || '';
 export async function fetchIpGeo(req: Request) {
   const params = new URLSearchParams();
   params.append(keyName, keyValue);
-  params.append('ip', req.header('X-Real-IP') || req.ip);
+  params.append('ip', req.header('X-Real-IP') || req.ip || '');
 
   const ipgeoData = await axios.get<unknown>(`${ipgeoURL}?${params}`);
 
