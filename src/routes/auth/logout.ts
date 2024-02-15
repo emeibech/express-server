1127b4d7f33bf7f6be216c136bde4d664f02b1eb
emeibech <98073805+emeibech@users.noreply.github.com>
2024-02-16 05:58:20 +0800

    Delete refresh token and clear cookie in logout

--- src/routes/auth/logout.ts
+++ src/routes/auth/logout.ts
@@ -1,40 +1,25 @@
 import { Router } from 'express';
-import { decodeToken, verifyToken } from '@/common/utils.js';
 import { transaction } from '@/database/utils.js';
 import logError from '@/common/logError.js';
+import { decodeToken } from '@/common/utils.js';
+import { handleAccess } from '@/common/middleWares.js';
 
 const logout = Router();
 
+logout.use(handleAccess);
 logout.post('/', async (req, res) => {
   try {
-    const token = req.headers.authorization;
+    const act = req.signedCookies.act;
+    const { rft } = decodeToken(act);
 
-    if (!token) {
-      return res.status(400).json({ message: 'Incomplete parameter.' });
-    }
+    await transaction([
+      {
+        text: 'DELETE FROM refresh_tokens WHERE token = $1',
+        values: [rft],
+      },
+    ]);
 
-    const { payload, expired } = await verifyToken(token);
-
-    if (expired) {
-      const { sid } = decodeToken(token);
-
-      await transaction([
-        {
-          text: 'DELETE FROM sessions WHERE id = $1',
-          values: [sid],
-        },
-      ]);
-    }
-
-    if (payload) {
-      await transaction([
-        {
-          text: 'DELETE FROM sessions WHERE id = $1',
-          values: [payload.sid],
-        },
-      ]);
-    }
-
+    res.clearCookie('act');
     res.status(200).json({ message: 'Log out successful.' });
   } catch (error) {
     res.status(500).json({ message: error });
